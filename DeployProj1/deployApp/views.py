157963426f632ce9aaa461cdae26aef5fec9acda
RajeshCena <llrajrajl@gmail.com>
2023-07-15 16:24:30 -1200

    commit-1 done Maya

---
+++ DeployProj1/deployApp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def display(request):
+    s1='<h1>Hello World</h1>'
+    s2='<h1>Good Evening</h1>'
+    s3='<h1>ITVEDANT</h1>'
+    return HttpResponse(s)
